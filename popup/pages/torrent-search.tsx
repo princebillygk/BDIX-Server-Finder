c1cb6c8eeec7348a7a968a1d68b7701cb92c8b20
princebillygk <princebillygk@gmail.com>
2023-07-19 17:45:52 +0600

    Add torrent search functionality and display results

--- popup/pages/torrent-search.tsx
+++ popup/pages/torrent-search.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 
 import InnerPageLayout from "../components/Layouts/InnerPageLayout"
@@ -7,19 +8,37 @@ export default function TorrentSearchPage() {
   const location = useLocation()
   const navigate = useNavigate()
 
+  const [torrentList, setTorrentList] = useState<string[]>([])
+
   async function handleSearch(searchTerm: string) {
     const response = await fetch(
       "https://btdig.com/search?" +
         new URLSearchParams({
-          q: searchTerm
+          q: searchTerm,
+          p: "0"
         })
     )
-    console.log(response.body)
+
+    const parser = new DOMParser()
+    const htmlDoc = parser.parseFromString(await response.text(), "text/html")
+    const results = htmlDoc.getElementsByClassName("one_result")
+
+    const torrents = []
+    for (const r of results) {
+      torrents.push(r.querySelector("div.torrent_name>a").textContent)
+    }
+
+    setTorrentList(torrents)
   }
 
   return (
     <InnerPageLayout title="Search torrents">
-      <Search onSearch={handleSearch} />
+      <>
+        <Search onSearch={handleSearch} />
+        {torrentList.map((t) => (
+          <a>{t}</a>
+        ))}
+      </>
     </InnerPageLayout>
   )
 }
